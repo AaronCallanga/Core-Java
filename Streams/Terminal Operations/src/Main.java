39d07a33660b52613cbdcb5c73277703930ebe5b
pogi <aaroncallanga01@gmail.com>
2025-11-01 20:01:53 +0800

    groupingByWithDownstreamCollectors - Streams

--- Streams/Terminal Operations/src/Main.java
+++ Streams/Terminal Operations/src/Main.java
@@ -1,6 +1,7 @@
 import java.util.ArrayDeque;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.Deque;
 import java.util.DoubleSummaryStatistics;
 import java.util.IntSummaryStatistics;
@@ -21,6 +22,7 @@ public class Main {
             new Employee("HR", "Charizard", 50000L),
             new Employee("IT", "Snorlax", 70000L),
             new Employee("Finance", "Bulbasaur", 40000L));
+            //new Employee("Finance", "Bulbasaur", 40000L));   Test it for Set collections
 
 
     public static void main(String[] args) {
@@ -34,11 +36,71 @@ public class Main {
 
         //partitioningBy();
         //groupingBy();
+        groupingByWithDownstreamCollectors();
         //reduce();
         //basicCollectors();
         //joining();
         //counting();
         //summarizingType();
+
+
+    }
+
+    private static void groupingByWithDownstreamCollectors() {
+
+        // Grouping + Mapping (w/ Collections)
+        // Can also do .toSet() to return unique items depending on the mapper(first args of .mapping())
+        // or Collectors.toCollection(Supplier<C> collectionFactory) such as (Collectors.toCollection(ArrayDeque::new)
+        // Collects the elements within each group into a List, Set, or a specific type of Collection.
+        Map<String, List<String>> listOfEmployeeNamesPerDepartment = employees.stream()
+                                                                              .collect(Collectors.groupingBy(Employee::department, Collectors.mapping(Employee::name, Collectors.toList())));
+        System.out.println("Grouping + Mapping -> List of Employees' Names Per Department: " + listOfEmployeeNamesPerDepartment);
+
+        // Grouping + Mapping (w/ Joining)
+        // Concatenates CharSequence elements within each group into a single String, separated by the specified delimiter.
+        Map<String, String> joinedEmployeeNamePerDepartment = employees.stream()
+                                                                       .collect(Collectors.groupingBy(Employee::department, Collectors.mapping(Employee::name, Collectors.joining(", "))));
+        System.out.println("Grouping + Joining -> List of Employees' Names Per Department: " + joinedEmployeeNamePerDepartment);
+
+        // Grouping + Mapping (w/ reduction)
+        // Performs a general reduction operation on the elements within each group using a BinaryOperator.
+        Map<String, Optional<String>> longestNameByDepartment = employees.stream()
+                                                                         .collect(Collectors.groupingBy(Employee::department,
+                                                                                 Collectors.mapping(Employee::name, Collectors.reducing((name1, name2) -> name1.length() > name2.length() ? name1 : name2))));
+        System.out.println("Grouping + Mapping (w/ reduction) -> Longest Employees' Name Per Department: " + longestNameByDepartment);
+
+        // Grouping + Counting
+        // Counts the number of elements within each group created by groupingBy()
+        Map<String, Long> numberOfEmployeesPerDepartment = employees.stream()
+                .collect(Collectors.groupingBy(Employee::department, Collectors.counting()));
+        System.out.println("Grouping + Counting -> Number of Employees Per Department: " + numberOfEmployeesPerDepartment);
+
+        // Grouping + MaxBy/MinBy
+        // Finds the maximum or minimum element within each group based on a provided Comparator.
+        Map<String, Optional<Employee>> highestSalaryPerDepartment = employees.stream()
+                .collect(Collectors.groupingBy(Employee::department, Collectors.maxBy(Comparator.comparing(Employee::salary))));
+        System.out.println("Grouping + MaxBy -> Employee Who Got Highest Salary Per Department: " + highestSalaryPerDepartment);
+
+        // Grouping + Summing (sum) (summingInt(), summingLong(), summingDouble())
+        // Computes the sum of a numeric-valued function applied to the elements within each group.
+        Map<String, Long> totalSalaryPerDepartment = employees.stream()
+                .collect(Collectors.groupingBy(Employee::department, Collectors.summingLong(Employee::salary)));
+        System.out.println("Grouping + Summing -> Total Salary Per Department: " + totalSalaryPerDepartment);
+
+        // Grouping + Averaging (average) (averagingInt(), averagingLong(), averagingDouble())
+        // Computes the average of a numeric-valued function applied to the elements within each group.
+        Map<String, Double> averageSalaryPerDepartment = employees.stream()
+                .collect(Collectors.groupingBy(Employee::department, Collectors.averagingDouble(Employee::salary)));
+        System.out.println("Grouping + Summing -> Average Salary Per Department: " + averageSalaryPerDepartment);
+
+        // Grouping + CollectingAndThen
+        // Applies a finishing transformation to the result of another downstream collector.
+        Map<String, List<String>> listOfNamesInUpperCasePerDepartment = employees.stream()
+                .collect(Collectors.groupingBy(Employee::department,
+                        Collectors.collectingAndThen(Collectors.mapping(Employee::name,
+                                Collectors.toList()),
+                                list -> list.stream().map(String::toUpperCase).collect(Collectors.toList()))));
+        System.out.println("Grouping + CollectingAndThen -> Uppercase Names Per Department: " + listOfNamesInUpperCasePerDepartment);
 
     }
 
@@ -127,28 +189,33 @@ public class Main {
     }
 
     // Group by whatever the parameter you passed (usually a property of an object) (a function that returns a value)
-    // returns Map of <FunctionType, List<Type>>/Map/Set or,   FunctionType - the args you passed in the groupingBy()
+    // returns Map of <FunctionType, List<Type>>/Map/Set        FunctionType - the args you passed in the groupingBy()
     private static void groupingBy() {
         Map<String, List<Employee>> groupedEmployees = employees.stream()
                                                        .collect(Collectors.groupingBy(Employee::department));
+        System.out.println("String, List<Employee> " + groupedEmployees);
 
-        Map<String, Set<Employee>> groupedEmployee2 = employees.stream()
+        Map<String, Set<Employee>> groupedEmployees2 = employees.stream()
                                                                .collect(Collectors.groupingBy(Employee::department, Collectors.mapping(Function.identity(), Collectors.toSet())));
+        System.out.println("String, Set<Employee> " + groupedEmployees2);
 
         Map<String, Map<String, Long>> groupedEmployees3 = employees.stream()
                                                           .collect(Collectors.groupingBy(Employee::department, Collectors.toMap(Employee::name, Employee::salary)));
+        System.out.println("String, Map<String, Long> " + groupedEmployees3);
 
         groupedEmployees.forEach((department, employees) -> {
             System.out.println(department);
             employees.forEach(System.out::println);
             //System.out.println(employees);
         });
+
         System.out.println("Employees' department = HR");
         groupedEmployees.get("HR").forEach(System.out::println);
         System.out.println("Employees' department = IT");
         groupedEmployees.get("IT").forEach(System.out::println);
         System.out.println("Employees' department = Finance");
         groupedEmployees.get("Finance").forEach(System.out::println);
+
         System.out.println(groupedEmployees);
         /*
         {
