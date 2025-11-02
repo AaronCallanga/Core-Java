c75315a2aa20446cc028667971ea182fb6cf9e78
pogi <aaroncallanga01@gmail.com>
2025-11-02 23:48:17 +0800

    dsa set up

---
+++ DSA/Sorting Algorithm/src/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+    public static void main(String[] args) {
+        bubbleSort();
+    }
+
+
+
+    // Bubble
+    private static void bubbleSort() {
+        int[] array = {1,5,9,2,3,8,7};
+        for (int i = 0; i < array.length - 1; i++) {
+
+        }
+    }
+    // Insertion
+    private static void insertionSort() {
+    }
+    // Selection
+    private static void selectionSort() {
+    }
+    // Merge
+    private static void mergeSort() {
+    }
+    // Quick
+    private static void quickSort() {
+    }
+}
