63d0c74c84af706c35db14688b73fe94b6534bbe
pogi <aaroncallanga01@gmail.com>
2025-11-08 12:35:10 +0800

    Generate the first 10 numbers of the Fibonacci Sequence

--- Streams/Practice/src/Main.java
+++ Streams/Practice/src/Main.java
@@ -17,9 +17,18 @@ public class Main {
 
     public static void main(String[] args) {
 
-        ex33();
+        ex34();
         //groupByName();
 
+    }
+
+    // Generate the first 10 numbers of the Fibonacci Sequence
+    private static void ex34() {
+        List<Integer> collect = Stream.iterate(new int[]{0, 1}, f -> new int[]{f[1], f[0] + f[1]})
+                                      .limit(10)
+                                      .map(arr -> arr[0])
+                                      .collect(Collectors.toList());
+        System.out.println(collect); // [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
     }
 
     // Find the occurrence of domains using Java streams
