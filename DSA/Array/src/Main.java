12da76b2f7cf8b9b33703fb890a9a832c6c19e4e
pogi <aaroncallanga01@gmail.com>
2025-11-12 22:06:09 +0800

    Rotate an Array by d - Counterclockwise or Left

--- DSA/Array/src/Main.java
+++ DSA/Array/src/Main.java
@@ -2,7 +2,26 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        p1();
+        p2();
+    }
+
+    // Rotate an Array by d - Counterclockwise or Left
+    private static void p2() {
+        int arr[] = {1, 2, 3, 4, 5, 6};
+        int n = 2;
+
+
+        for (int i = 0; i < n; i++) {
+            // Take note of the first element
+            int firstElement = arr[0];
+            for (int j = 1; j < arr.length; j++) {
+                // Shift the element after the first element
+                arr[j - 1] = arr[j];
+            }
+            // Place it at the very last
+            arr[arr.length - 1] = firstElement;
+        }
+        System.out.println(Arrays.toString(arr)); // [3, 4, 5, 6, 1, 2]
     }
 
     // Reverse an Array in groups of given size
@@ -10,12 +29,10 @@ public class Main {
         int[] arr = {1, 2, 3, 4, 5, 6, 7, 8};
         int k = 3;
 
-        int left;
-        int right;
 
-        for(int i = 0; i < arr.length; i+= k) {
-            left = i;
-            right = Math.min(i + k, arr.length - 1); // To prevent index out of bound in last iteration if k is not a multiple of arr.length (e.g k = 3, n = 5 -> 2 excess)
+        for(int i = 0; i < arr.length; i += k) {
+            int left = i;
+            int right = Math.min(i + k - 1, arr.length - 1); // To prevent index out of bound in last iteration if k is not a multiple of arr.length (e.g k = 3, n = 5 -> 2 excess)
 
             while (left < right) {
                 int temp = arr[left];
@@ -25,7 +42,7 @@ public class Main {
                 right--;
             }
         }
-        System.out.println(Arrays.toString(arr));
+        System.out.println(Arrays.toString(arr)); // [3, 2, 1, 6, 5, 4, 8, 7]
     }
 
 
